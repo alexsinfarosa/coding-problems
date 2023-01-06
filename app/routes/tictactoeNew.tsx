2c811bb072e1f7171d4cd5f3512ba3b84cd85ef0
Alex Sinfarosa <alexsinfarosa@users.noreply.github.com>
2023-01-06 13:48:32 +0100

    winner has colored squares

--- app/routes/tictactoeNew.tsx
+++ app/routes/tictactoeNew.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import React from 'react'
 
 export default function TicTacToe() {
@@ -5,15 +6,8 @@ export default function TicTacToe() {
     Array(9).fill(null),
   )
   const [isX, setIsX] = React.useState(false)
-  console.log(squares, isX)
 
   const winner = calculateWinner(squares)
-  let status
-  if (winner) {
-    status = `Winner is: ${winner}`
-  } else {
-    status = `Next Player: ${isX ? 'O' : 'X'}`
-  }
 
   function handleClick(i: number) {
     if (winner) return
@@ -39,36 +33,42 @@ export default function TicTacToe() {
 
   return (
     <main className="flex flex-col justify-center items-center h-screen">
-      <h2 className="text-4xl font-semibold mb-4">{status}</h2>
-      <section className="grid grid-cols-3 grid-rows-3">
-        <Square i={0} onSquareClick={handleClick}>
-          {squares[0]}
-        </Square>
-        <Square i={1} onSquareClick={handleClick}>
-          {squares[1]}
-        </Square>
-        <Square i={2} onSquareClick={handleClick}>
-          {squares[2]}
-        </Square>
-        <Square i={3} onSquareClick={handleClick}>
-          {squares[3]}
-        </Square>
-        <Square i={4} onSquareClick={handleClick}>
-          {squares[4]}
-        </Square>
-        <Square i={5} onSquareClick={handleClick}>
-          {squares[5]}
-        </Square>
-        <Square i={6} onSquareClick={handleClick}>
-          {squares[6]}
-        </Square>
-        <Square i={7} onSquareClick={handleClick}>
-          {squares[7]}
-        </Square>
-        <Square i={8} onSquareClick={handleClick}>
-          {squares[8]}
-        </Square>
+      {winner ? (
+        <h2 className="text-4xl font-semibold mb-4">
+          Winner is <span className="text-red-500">{winner?.player}</span>
+        </h2>
+      ) : (
+        <h2 className="text-4xl font-semibold mb-4">
+          Next Player: {isX ? 'O' : 'X'}
+        </h2>
+      )}
+
+      <section className="grid grid-cols-3 grid-rows-3 mb-4">
+        {squares.map((square, i) => {
+          let isColored = false
+          if (winner) {
+            isColored = winner.line.includes(i)
+          }
+          return (
+            <Square
+              key={i}
+              i={i}
+              onSquareClick={handleClick}
+              isColored={isColored}
+            >
+              {square}
+            </Square>
+          )
+        })}
       </section>
+      {winner && (
+        <button
+          className="inline-flex items-center rounded-md border border-transparent bg-rose-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2"
+          onClick={() => setSquares(Array(9).fill(null))}
+        >
+          Reset
+        </button>
+      )}
     </main>
   )
 }
@@ -77,14 +77,19 @@ function Square({
   i,
   children,
   onSquareClick,
+  isColored,
 }: {
   i: number
   children: string | null
   onSquareClick: (i: number) => void
+  isColored: boolean | undefined
 }) {
   return (
     <button
-      className="w-28 h-28 border text-6xl font-bold"
+      className={clsx(
+        'w-28 h-28 border text-6xl font-bold',
+        isColored ? 'text-red-500' : '',
+      )}
       onClick={() => onSquareClick(i)}
     >
       {children}
@@ -107,7 +112,7 @@ function calculateWinner(squares: (string | null)[]) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i]
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+      return {player: squares[a], line: lines[i]}
     }
   }
 }
